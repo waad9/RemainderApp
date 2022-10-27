5b588f4125c25aa32ba26b552c44d4c0a81266ed
githup <waalruwaili22@twq.idserve.net>
2022-10-27 12:58:42 +0300

    Initial Commit

---
+++ 18/_8App.swift
@@ -0,0 +1,17 @@
+//
+//  _8App.swift
+//  18
+//
+//  Created by وعد عياد الرويلي on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct _8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
