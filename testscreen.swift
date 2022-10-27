5f284f853d44b7f9f025e333d2430b657b515c6c
githup <waalruwaili22@twq.idserve.net>
2022-10-27 13:00:27 +0300

    first

---
+++ testscreen.swift
@@ -0,0 +1,20 @@
+//
+//  testscreen.swift
+//  18
+//
+//  Created by وعد عياد الرويلي on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+struct testscreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct testscreen_Previews: PreviewProvider {
+    static var previews: some View {
+        testscreen()
+    }
+}
